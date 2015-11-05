904359f0e18fc83bcca3fe558332264f22a25a41
Sally Kingston <sally.a.kingston@gmail.com>
2015-11-05 15:36:46 -0500

    more client setup files

---
+++ client/app.js
@@ -0,0 +1,16 @@
+var menuPage = {
+  init: function (){
+    // display.init();
+    //edit.init();
+  },
+  events: function () {
+
+  },
+  styling: function (){
+
+  }
+}
+
+$(document).ready(function () {
+  menuPage.init();
+});
