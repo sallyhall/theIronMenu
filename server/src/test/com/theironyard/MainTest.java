06ea11aee22b81ec1ced667ff02cff6235aa60fd
BennettIronYard <bennettiy12@gmail.com>
2015-11-05 17:21:21 -0500

    Working on testing methods

--- server/src/test/com/theironyard/MainTest.java
+++ server/src/test/com/theironyard/MainTest.java
@@ -1,8 +1,11 @@
 package com.theironyard;
 
+import org.junit.Test;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import static org.junit.Assert.*;
 
@@ -12,9 +15,23 @@ import static org.junit.Assert.*;
 public class MainTest {
     public Connection startConnection() throws SQLException {
         Connection conn = DriverManager.getConnection("jdbc:h2:./test");
-        Main.createTables(conn);
+        Main.createTable(conn);
         return conn;
     }
-
+    public void endConnection(Connection conn) throws SQLException {
+        Statement stmt = conn.createStatement();
+        stmt.execute("DROP TABLE menu");
+        //stmt.execute("DROP TABLE menu");
+        conn.close();
+    }
+    @Test
+    public void testMenuItem() throws SQLException {
+        Connection conn = startConnection();
+        Main.insertMenuItem(conn, "Steak", "entree", true, true, true, 25.00, false, false, 2);
+        Main.insertMenuItem(conn,  "Salad", "app", false, true, true, 10.00, true, true, 1);
+        MenuItem menuItem = Main.selectMenuItem(conn, "Steak");
+        endConnection(conn);
+        assertTrue(menuItem != null);
+    }
 
 }
