350317447b2ae87efbed2402b275783dc9cbfd0c
Sally Kingston <sally.a.kingston@gmail.com>
2015-11-06 16:09:37 -0500

    started working on add with bear

--- client/app.js
+++ client/app.js
@@ -16,6 +16,18 @@ var menuPage = {
   currentDataSet: {},
 };
 
+var MenuItem = function(breakfast,dinner,glutenFree,id,lunch,name,price,type,vegetarian){
+  this.breakfast = breakfast;
+  this.dinner=dinner;
+  this.glutenFree=glutenFree;
+  this.id=id;
+  this.lunch=lunch;
+  this.name=name;
+  this.price=price;
+  this.priceRange=Math.floor(price/10) +1;
+  this.type=type;
+  this.vegetarian=vegetarian;
+};
 
 
 $(document).ready(function () {
